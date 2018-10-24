d659c1d6b457a9372be656848374998c3b926b40
Qasper <Christensen.H.Kasper@gmail.com>
2018-10-24 20:19:13 +0200

    Initial Commit

---
+++ HideAndFind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HideAndFind
+//
+//  Created by Kasper Christensen on 24/10/2018.
+//  Copyright © 2018 Kasper Christensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
